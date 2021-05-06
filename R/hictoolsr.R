f29a70db2303e072394a3315466ba7fcd6508b17
Eric Davis <ericscottdavis@outlook.com>
2021-05-06 16:46:55 -0400

    Fix imports
    extractCounts wasn't working without importing the entire GenomicRanges package

--- R/hictoolsr.R
+++ R/hictoolsr.R
@@ -5,13 +5,13 @@
 #' @rawNamespace import(data.table, except = c(between, shift, first, second))
 #' @rawNamespace import(grid)
 #' @rawNamespace import(BentoBox)
+#' @rawNamespace import(GenomicRanges)
 #' @rawNamespace import(InteractionSet)
 #' @rawNamespace import(plyranges)
 #' @importFrom strawr straw
 #' @importFrom tidyr complete full_seq
 #' @importFrom reshape2 acast
 #' @importFrom magrittr %<>%
-#' @importFrom GenomicRanges shift resize trim makeGRangesFromDataFrame seqnames
 #' @importFrom S4Vectors width mcols `mcols<-` first second DataFrame
 #'
 "_PACKAGE"
