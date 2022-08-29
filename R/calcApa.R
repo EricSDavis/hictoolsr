efbdda5593793a2788a42830900a494c8573abdd
Manjari KIran <56753695+ManjariKiran@users.noreply.github.com>
2022-08-29 13:41:33 -0400

    Update calcApa.R

--- R/calcApa.R
+++ R/calcApa.R
@@ -6,8 +6,9 @@
 #' @param res integer resolution of bedpe bins.
 #' @param buffer integer number of res-length bins from the center pixel.
 #' @param filter TRUE or FALSE (default). If TRUE, \code{filterBedpe()} will be used to remove short intrachromosomal interactions that would otherwise cross the diagonal.
-#'
-#' @return Returns a list APA matricies that can be compiled and plotted.
+#' @param matrix Type of matrix to output. Can be one of observed/oe/expected. observed is observed counts, oe is observed/expected counts, expected is expected counts.
+#' matrix <- match.arg(arg=matrix, choices =c("observed","expected","oe")) 
+#' @return Returts a list APA matricies that can be compiled and plotted.
 #'
 #'
 #' @export
